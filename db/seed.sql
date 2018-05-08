c7a3f09fededb7385c52b72675639b6d061021c9
aisha barrous <aishabarrous@ool-ad0200d7.dyn.optonline.net>
2018-05-08 14:16:58 -0400

    database tables done

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+
+
+
+INSERT INTO users (username, email, password, pic_url)
+VALUES ('username', 'email@.com', 'password', 'https://pbs.twimg.com/profile_images/2275441542/i3d5t1j5wtejm4qgb3kq_400x400.jpeg' );
+
+INSERT INTO favorites (drink_name, image_url, ingredients, instructions, user_id)
+VALUES ('drink_name', 'http://25.media.tumblr.com/729d8ed33887c79fe55efb39c57d81b4/tumblr_mg5wl7Wv8t1qgm4hdo1_250.jpg', 'ingredients', 'instructions', 1);
