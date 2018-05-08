941e7110cc4db9acf005c23d7777c1e77bf2b164
Stephanie B Burr <sbeusse@gmail.com>
2018-05-08 11:06:36 -0400

    Setup Express Files -NPM init for node_modules -Installed dependencies Express, Nodemon, Morgan, BodyParser -Set up server.js to use dependencies and listen to port -Successfully listening on port

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.send('Hey team DrinkUp!');
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
