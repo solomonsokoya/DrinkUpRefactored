297a8e53f31690cc00e58971a225d195bd10e57e
Solomon sokoya <solomonsokoya@ool-ad0202c0.dyn.optonline.net>
2018-05-10 17:37:04 -0400

    Conditonal Rendering in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
 import './App.css';
-import Nav from './components/Nav';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      currentUser: null
+    }
+  }
   render() {
-    return (
-      <div className="App">
-        <Nav />
+    const View;
+    if(this.state.currentUser === null){
+        View = (
+          <div>
+            <main>
+              <Route path="/login" component={Login}/>
+              <Route path="/register" component={Register}/>
+            </main>
+            <button className='Login'><Link to ='/login'>Login</Link></button>
+            <button className='Register'><Link to='/register'>Register</Link></button>
+          </div>
+          )
+      } else {
+
+      View = ( <div className="App">
+        <Profile/>
       </div>
-    );
+      )
+    };
+
+    return ({View});
+
   }
 }
 
