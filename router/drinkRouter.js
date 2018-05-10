44dcc2d38260becc60cb1632bcf7baf93afc638d
Solomon sokoya <solomonsokoya@ool-ad0202c0.dyn.optonline.net>
2018-05-10 11:38:32 -0400

    Errors on Put

--- router/drinkRouter.js
+++ router/drinkRouter.js
@@ -2,21 +2,21 @@ const drinksRouter = require('express').Router();
 const drinksController = require('../controllers/drinksController');
 const responseController = require('../controllers/responseController');
 
-drinksRouter.route('/')
-  .get(
-    drinksController.getAll,
-    responseController.sendOkResponse,
-    responseController.sendErrorResponse
-  )
-  .post(
-    drinksController.create,
-    responseController.sendOkResponse,
-    responseController.sendErrorResponse
-  );
+// drinksRouter.route('/')
+//   .get(
+//     drinksController.getAll,
+//     responseController.sendOkResponse,
+//     responseController.sendErrorResponse
+//   )
+//   .post(
+//     drinksController.create,
+//     responseController.sendOkResponse,
+//     responseController.sendErrorResponse
+//   );
 
 drinksRouter.route('/:id')
   .get(
-    drinksController.getOne,
+    drinksController.getAll,
     responseController.sendOkResponse,
     responseController.sendErrorResponse
   )
