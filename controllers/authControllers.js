d03bcc50a9852b4d280547084619013aa916423d
Aron Crawford <aroncrawford1029@yahoo.com>
2018-05-09 21:04:45 -0400

    aron authcontroller-please look at it lol

---
+++ controllers/authControllers.js
@@ -0,0 +1,60 @@
+const userModel = require('../models/drinks.js');
+
+function receiveToken(req, res, next) {
+  if (req.headers.authorization) {
+    req.authToken = req.headers.authorization.replace(/^Bearer\s/, '');
+  }
+  next();
+};
+
+function register(req, res) {
+  userModel.register(req.body)
+    .catch(err => res.status(401).json({
+      message: 'Username taken'
+    }))
+    .then(data => tokenService.makeToken({
+        email: data.email,
+        id: data.id
+    }))
+    .then(token => {
+      res.json({
+        token
+      })
+    });
+}
+
+function restrict(req, res, next) {
+    tokenService.verify(req.authToken)
+      .then(data => {
+        res.locals.user = data;
+        next();
+      })
+      .catch(err => res.status(401).json({
+        status: 'Error',
+        message: 'Invalid credentials'
+      }))
+  }
+
+function login(req, res, next) {
+  userModel.login(req.body)
+    .catch(err => res.status(401).json({
+      status: 'Error',
+      message: 'Invalid credentails'
+    }))
+    .then(data => tokenService.makeToken({
+      id: data.id,
+      email: data.email
+    }))
+    .then(token => {
+      res.json({
+        token
+      })
+    })
+}
+
+module.exports = {
+  receiveToken,
+  register,
+  restrict,
+  login
+}
