20a755cde868f866aa4978b051171e82ee9f6614
Solomon sokoya <solomonsokoya@Solomons-MacBook-Pro.local>
2018-05-09 20:15:14 -0400

    Merged controllers and fixed bugs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+const usersRouter = require('./router/userRouter')
+const drinksRouter = require('./router/drinkRouter')
+const authRouter = require('./router/authRouter')
 
 const app = express();
 
@@ -14,5 +16,10 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', (req, res) => {
   res.send('Hey Team DrinkUp!');
 })
+
+app.use('/users', usersRouter);
+app.use('/drinks', drinksRouter);
+app.use('/auth', authRouter)
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
