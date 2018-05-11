4bfaa7e2150fb988d95f6319274709df9c5930a8
Solomon sokoya <solomonsokoya@Solomons-MBP.fios-router.home>
2018-05-10 22:20:38 -0400

    added login and token function. added props to Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
+import jwt from 'jwt-js';
 import Register from './components/Register';
 import Profile from './components/Profile';
 import Home from './components/Home';
@@ -14,6 +15,64 @@ class App extends Component {
       currentUser: null
     }
   }
+
+checkToken() {
+    const authToken = localStorage.getItem('authToken');
+    fetch('/auth', {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${authToken}`
+      }
+    })
+      .then(resp => {
+        if (!resp.ok) throw new Error(resp.message);
+        return resp.json()
+      })
+      .then(respBody => {
+        this.setState({
+          currentUser: respBody.user
+        })
+      })
+      .catch(err => {
+        console.log('not logged in');
+        localStorage.removeItem('authToken');
+        this.setState({
+          currentUser: null
+        });
+      })
+  }
+
+  loginRequest(attempt) {
+    console.log('attempting to log in with attempt:');
+    console.log(attempt);
+    fetch('/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(attempt),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+      .then(resp => {
+        if (!resp.ok) throw new Error(resp.statusMessage);
+        return resp.json();
+      })
+      .then(respBody => {
+        console.log(respBody);
+        localStorage.setItem('authToken', respBody.token)
+        this.setState({
+          currentUser: jwt.decodeToken(respBody.token).payload
+        })
+      })
+  }
+
+handleLogin(attempt){
+  this.loginRequest(attempt)
+}
+componentDidMount(){
+  this.checkToken();
+}
+
   render() {
     let View;
     if(this.state.currentUser === null){
@@ -22,8 +81,8 @@ class App extends Component {
             <main>
             <Switch>
               <Route exact path= "/" component= {Home} />
-              <Route path="/login" component={Login}/>
-              <Route path="/register" component={Register}/>
+              <Route path="/login" component={() => (<Login onLogin = {this.handleLogin}/>)}/>
+              <Route path="/register" component={() => (<Register onLogin = {this.handleRegister}/>)}/>
             </Switch>
             </main>
           </div>
