c6f9cd61fe3facde9190579d4282e81ab7590627
Stephanie B Burr <sbeusse@gmail.com>
2018-05-11 14:24:44 -0400

    merging in working Profile component

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
 export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: '',
+      username: '',
+      pic_url: ''
+    }
+
+  }
+
+  componentDidMount() {
+    if (this.props.user) {
+      this.setState({
+        id: this.props.user.id,
+        username: this.props.user.username,
+        pic_url: this.props.user.pic_url
+      })
+    }
+  }
   render() {
     return (
-      <div>I am the profile page</div>
+      <div>
+        <h2>Profile Name: {this.props.user.username}</h2>
+      </div>
       )
 
   }
