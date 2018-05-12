b22873dccfeb99e6f6fc4bc251b868ba63509301
Stephanie B Burr <sbeusse@gmail.com>
2018-05-12 12:42:23 -0400

    Merging from profileRenders branch
    -added the rest of the props
    -everything showing up and minimally styled for easy viewing
    -added dummy "edit" and "delete" buttons

--- client/src/components/FavDrinks.jsx
+++ client/src/components/FavDrinks.jsx
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
 
+const drinkImg = {
+  width: "20%"
+}
+
+const drinkContainer ={
+  border: "solid 2px black",
+  padding: "10px",
+  margin: "20px"
+}
 
 
 class FavDrinks extends Component {
@@ -13,30 +22,18 @@ class FavDrinks extends Component {
   render() {
     if(this.props.drinks !== 0){
       return (this.props.drinks.map(drink => (
-      <div key={drink.drink_id}>
-        <h1>{drink.drink_name}</h1>
-        <h2>{drink.ingredients}</h2>
+      <div key={drink.drink_id} style={drinkContainer}>
+        <img src={drink.image_url} alt="" style={drinkImg}/>
+        <button>EDIT DRINK</button>
+        <h3>{drink.drink_name}</h3>
+        <h4>{drink.ingredients}</h4>
+        <h4>{drink.instructions}</h4>
+        <button>DELETE DRINK</button>
       </div>
     )))} else {
      return (<p>Loading</p>)
     }
-
   }
 }
-
-
-// {this.props.drinks.map(drink => (
-//       <div key={drink.drink_id}>
-//       <h1>
-//       {drink.drink_name}
-//       </h1>
-
-//       <h2>
-//       {drink.ingredients}
-//       </h2>
-//       </div>
-
-//       )
-//       )}
 
 export default FavDrinks;
