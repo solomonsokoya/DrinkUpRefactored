e32ff453565c0b742d984c4874eb335565a30463
Solomon sokoya <solomonsokoya@ool-ad0202c0.dyn.optonline.net>
2018-05-15 16:59:19 -0400

    Added Nav Style Sheet and Logo

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import '../Nav.css'
 
 //todo add drink iconography into nav bar
 export default class Nav extends Component {
@@ -12,15 +13,16 @@ export default class Nav extends Component {
 
   render() {
     return (
-      <nav>
+
+      <nav className = 'navContainer'>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"/>
         <ul>
-          <li><Link to="/">MY DRINKS</Link></li>
-          <li><Link to="/drinks">FIND DRINKS</Link></li>
-          <li><Link to="/drinks/new">MAKE A DRINK</Link></li>
-          <li><Link to="/" onClick={this.logoutRequest.bind(this)}>LOGOUT</Link></li>
+          <li><Link style={{ textDecoration: 'none', color: 'black' }} to="/"><i class="fas fa-glass-martini"></i></Link></li>
+          <li><Link style={{ textDecoration: 'none', color: 'black' }} to="/drinks">FIND DRINKS</Link></li>
+          <li><Link style={{ textDecoration: 'none', color: 'black' }} to="/" onClick={this.logoutRequest.bind(this)}>LOGOUT</Link></li>
         </ul>
       </nav>
       )
   }
 }
-          // <li><Link to="">HAPPY HOUR</Link></li>
+
